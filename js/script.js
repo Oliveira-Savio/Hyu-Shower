41a918e30f45a99ee1676a80bab12fcbd0402133
Oliveira-Savio <savio.naoliveira@gmail.com>
2023-05-29 17:35:04 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+<script>
+const hyu = document.querySelector('.hyu');
+
+const jump = () => {
+    hyu.classList.add('jump');
+
+    setTimeout(() => {
+
+        hyu.classList.remove('jump');
+
+    }, 500);
+
+} 
+
+document.addEventListener('keydown', jump);
+</script>
+*/
